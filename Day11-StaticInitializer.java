a4481c6db6f9dba809da2bbbfdc98d78e81797a9
MayElfkiy <Mayelfkiy@gmail.com>
2017-04-26 06:06:12 +0200

    Create Day11-StaticInitializer.java

---
+++ Day11-StaticInitializer.java
@@ -0,0 +1,37 @@
+//Java's static initialization block
+package staticinitializer;
+
+import java.util.Scanner;
+
+public class StaticInitializer {
+  static int B,H;
+  static boolean flag;
+    
+  static{
+   
+   Scanner scan= new Scanner(System.in);
+   B = scan.nextInt();
+   H =scan.nextInt();
+   
+    if(B>0 && H>0){
+        flag= true;
+    }
+    
+    else{
+     System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+    
+   }
+    
+    public static void main(String[] args) {
+      
+        if (flag){
+        int area=B*H;
+       
+        System.out.println(area);
+        }
+               
+    }
+}
+    
+
