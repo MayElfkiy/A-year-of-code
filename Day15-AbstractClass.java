7cbdffbdcd4bf1ac91c18f8b9a1dd7aa4bdd71e3
MayElfkiy <Mayelfkiy@gmail.com>
2017-04-29 22:41:16 +0200

    Abstract class inheritance in java

---
+++ Day15-AbstractClass.java
@@ -0,0 +1,46 @@
+
+
+abstract class Book {
+    String title;
+    String author;
+    
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    abstract void display();
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Makes a MyBook sub-class to inherit from Book super class
+//Makes a display() method to print respective book title, author and price
+class MyBook extends Book{
+        int price;
+        public MyBook(String title, String author, int price){
+        super(title, author);
+        this.price= price;
+        this.title= title;
+        this.author= author;}
+    
+    void display(){
+    System.out.println("Title: "+ title);
+    System.out.println("Author: "+ author);
+    System.out.println("Price: "+ price);
+    
+    
+}
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public class Solution {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
