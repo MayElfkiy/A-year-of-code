03ee572797eb4e66ed02adf2008fce774ba20cfc
MayElfkiy <Mayelfkiy@gmail.com>
2017-04-30 20:29:33 +0200

    Update Day14-Inheritance.java

--- Day14-Inheritance.java
+++ Day14-Inheritance.java
@@ -1,3 +1,23 @@
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate() );
+	}
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 class Person {
 	protected String firstName;
 	protected String lastName;
@@ -18,7 +38,7 @@ class Person {
 	}
 
 }
-/////////////////////////////////////////////////////////////////////////////////////////////////
+
 class Student extends Person{
 }
     public char calculate(){
@@ -45,21 +65,3 @@ class Student extends Person{
 }
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////
-class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String firstName = scan.next();
-		String lastName = scan.next();
-		int id = scan.nextInt();
-		int numScores = scan.nextInt();
-		int[] testScores = new int[numScores];
-		for(int i = 0; i < numScores; i++){
-			testScores[i] = scan.nextInt();
-		}
-		scan.close();
-
-		Student s = new Student(firstName, lastName, id, testScores);
-		s.printPerson();
-		System.out.println("Grade: " + s.calculate() );
-	}
-}
